1f8811f110413c6e33451238cf0f9acffca19017
Anna <anna.dai@duke.edu>
2022-12-11 02:57:37 -0500

    update mlp

--- mlp.py
+++ mlp.py
@@ -9,25 +9,20 @@ import torch.nn as nn
 
 
 class LinearLayer(nn.Module):
-    def __init__(self, in_features, out_features, bias=True, bnorm=False, **kwargs):
-        super(LinearLayer, self).__init__(**kwargs)
+    def __init__(self, in_features, out_features, use_bias=False, use_bn=False):
+        super(LinearLayer, self).__init__()
 
-        self.in_features = in_features
-        self.out_features = out_features
-        self.bias = bias
-        self.bnorm = bnorm
-
+        self.use_bn = use_bn
         self.linear = nn.Linear(
-            in_features=self.in_features,
-            out_features=self.out_features,
-            bias=self.bias and not self.bnorm,
+            in_features=in_features,
+            out_features=out_features,
+            bias=use_bias and not use_bn,
         )
-        if self.bnorm:
-            self.bnorm = nn.BatchNorm1d(self.out_features)
+        self.bn = nn.BatchNorm1d(self.out_features)
 
     def forward(self, x):
         x = self.linear(x)
-        self.bnorm(x) if self.bnorm else x
+        x = self.bn(x) if self.use_bn else x
         return x
 
 
@@ -38,35 +33,30 @@ class ProjectionHead(nn.Module):
         hidden_features,
         out_features,
         head_type="nonlinear",
-        **kwargs
     ):
-        super(ProjectionHead, self).__init__(**kwargs)
-        self.in_features = in_features
-        self.out_features = out_features
-        self.hidden_features = hidden_features
-        self.head_type = head_type
+        super(ProjectionHead, self).__init__()
 
-        if self.head_type == "linear":
+        if head_type == "linear":
             self.layers = LinearLayer(
-                in_features=self.in_features,
-                out_features=self.out_features,
-                bias=False,
-                bnorm=True,
+                in_features=in_features,
+                out_features=out_features,
+                use_bias=False,
+                use_bn=True,
             )
-        elif self.head_type == "nonlinear":
+        elif head_type == "nonlinear":
             self.layers = nn.Sequential(
                 LinearLayer(
-                    in_features=self.in_features,
-                    out_features=self.hidden_features,
-                    bias=True,
-                    bnorm=True,
+                    in_features=in_features,
+                    out_features=hidden_features,
+                    use_bias=True,
+                    use_bn=True,
                 ),
                 nn.ReLU(),
                 LinearLayer(
-                    in_features=self.hidden_features,
-                    out_features=self.out_features,
-                    bias=False,
-                    bnorm=True,
+                    in_features=hidden_features,
+                    out_features=out_features,
+                    use_bias=False,
+                    use_bn=True,
                 ),
             )
 
